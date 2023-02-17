8e2ab1d3694391964dce7eb46e335a71a0112104
Eric Riddoch <eric.riddoch@bengroup.com>
2023-02-17 11:55:34 -0700

    wrote pyproject.toml to replace linting files

--- rest-api/src/example_rest_api/errors.py
+++ rest-api/src/example_rest_api/errors.py
@@ -1,7 +1,39 @@
-from typing import Dict
+from typing import (
+    Dict,
+    Optional,
+    Union,
+)
 
-from fastapi import HTTPException
-from starlette.status import HTTP_404_NOT_FOUND
+from fastapi import (
+    HTTPException,
+)
+from starlette.status import (
+    HTTP_404_NOT_FOUND,
+)
+
+aaaa_aaaa_aaaa_aaaa = 1
+
+hhhhlkjkljlkjlkjljlkjljlkjlkjlkjlkjlkjlkjlkjlkjlkjlkjljljlkjlkjlkjlkjlkjlkjlkjlkjlkjlkjlkjlkjlkjlkjlkjlkjlkjlkjlkjlkjlkjlkasdfasdfasdfasdfasdfasdfasdfasdasdfsadfasdfasdfasdf = (
+    1
+)
+
+a = 1
+b = 2
+c = 3
+d = 4
+e = 5
+f = 6
+g = 7
+h = 8
+i = 9
+j = 10
+
+
+def func():
+    for i in range(a):
+        if a + b > c and b + c > d and c + d > e and d + e > f and e + f > g and f + g > h and g + h > i:
+            if b + c > d and c + d > e and d + e > f and e + f > g and f + g > h and g + h > i:
+                print("hi")
 
 
 class Error(Exception):
@@ -12,16 +44,37 @@ class FileNotFoundError(Error):
     """Error returned by internal functions when a file cannot be found."""
 
     @staticmethod
-    def make_message(path: str) -> str:
+    def make_message(
+        path: str,
+    ) -> str:
         """Return a message for the exception."""
         return f"Could not find file: {path}"
 
     @staticmethod
-    def make_http_exception(path: str) -> HTTPException:
-        """Return an HTTPException for the exception."""
-        return HTTPException(status_code=HTTP_404_NOT_FOUND, detail=FileNotFoundError.make_message(path))
+    def make_http_exception(
+        path: str,
+    ) -> HTTPException:
+        """
+        Return an HTTPException for the exception.
+
+        A-hoy there!
+
+        Parameters
+        ----------
+
+        paths: str
+            The path to the file that could not be found.
+
+        :param paths: hi
+        """  # noqa: D413,D417
+        return HTTPException(
+            status_code=HTTP_404_NOT_FOUND,
+            detail=FileNotFoundError.make_message(path),
+        )
 
     @staticmethod
-    def make_http_exception_dict(path: str) -> Dict[str, str]:
+    def make_http_exception_dict(
+        path: str,
+    ) -> Dict[str, str]:
         """Return an HTTPException for the exception."""
         return {"detail": FileNotFoundError.make_message(path)}
